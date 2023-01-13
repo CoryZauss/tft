6a15d4862679b6823a957ab3d84ac5585845f0c5
Cory Zauss <coryclassic1989@gmail.com>
2023-01-13 14:42:42 -0500

    add some components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="flex items-center flex-col text-center pt-20 pb-6 text-4xl md:text-7xl mb-1 md:mb-3 font-bold">
+      TFT APP
+    </div>
+  );
+}
