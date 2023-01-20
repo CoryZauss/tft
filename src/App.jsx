e64c89aae1862f98322624a735810c46faec7d03
Cory Zauss <coryclassic1989@gmail.com>
2023-01-19 19:09:28 -0500

    add footer disclaimer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import './styles/tailwind.css'
 import History from './components/History.jsx'
 import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
         <History />
       </div>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="flex items-center flex-col text-center pt-5 mb-6 text-xs">
+      {
+        "TFT APP is not endorsed by Riot Games and does not reflect the views or opinions of Riot Games or anyone officially involved in producing or managing Riot Games properties. Riot Games and all associated properties are trademarks or registered trademarks of Riot Games, Inc"
+      }
+    </div>
+  );
+}
